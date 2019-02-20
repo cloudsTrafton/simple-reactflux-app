8cbddcbbcd68dbabe1d055bc784369f75430808e
cloudsTrafton <traftonator@gmail.com>
2019-02-20 16:32:44 -0500

    Fixes to pass tests, video demos

--- tests/content.spec.js
+++ tests/content.spec.js
@@ -15,10 +15,19 @@ module.exports = {
             .assert.containsText('#hungry-button', 'Hungry?');
         client.end();
     },
-    'When I click the "Hungry?" button, the text changes to "Grab a Snickers"'(client) {
+    'When I click the "Hungry?" button, the text changes to "We have vegetables at home. :("'(client) {
         client
             .url(url)
             .waitForElementVisible('#hungry-button')
+            .click('#hungry-button')
+            .assert.containsText('#hungry-button', 'We have vegetables at home. :(');
+        client.end();
+    },
+    'When I click the "Hungry?" button, the text changes to "Grab a Snickers!"'(client) {
+        client
+            .url(url)
+            .waitForElementVisible('#hungry-button')
+            .click('#hungry-button')
             .click('#hungry-button')
             .assert.containsText('#hungry-button', 'Grab a Snickers!');
         client.end();
